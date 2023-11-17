e1780c360d37945e8f28af5e2a6790726b9e1d07
Champa <champa@lexaeon.com>
2023-11-17 04:24:08 -0500

    lesson 45 done

--- Notes.php
+++ Notes.php
@@ -42,7 +42,7 @@ Lesson objectives:
 ==================================
 Lesson objectives:
 1. How to create a custom post type named 'Program'
-2. How to create relationship between two custom post types 
+2. How to create relationship between two custom post types by creating relation field and bringing them to front end
 ---------------
 For lesson objective 1(lessn 36):
 1. To create a custom post type we added a set of code in must plugin file 
@@ -50,7 +50,13 @@ For lesson objective 1(lessn 36):
 3. We created a related field in event post to link program post
 ---------------
 For lesson objective 2(lessn 37):
-
+1. To create a custom post type we added a set of code in must plugin file 
+2. We created a single page and a archive page for the new custom post type
+3. We created a related field in event post to link program post in event post page
+4. We added a filter to 2 upcoming post(already used in home page) to show program related post
+---------------
+For lesson objective 2(lessn 37):
+1. Link program page to the menu
 
 //We learnt in section 9
 ==================================
@@ -62,7 +68,17 @@ Lesson objectives:
 For lesson objective 1(lessn 40):
 1. To create a custom post type we added a set of code in must plugin file 
 2. We created a single page template for it
-3. We created a related field in event post to link program post
+3. We linked the previously created relation field for event
 4. We created a relation to Program post type in single-program.php file
 --------------
 Lesson 43: How to make page banner background dynamic
+
+//We learnt in section 10
+==================================
+Lesson 45. we are trying to create a dynamic page banner for all pages and custom post types
+   i. We created a function called pageBanner in function.php which will work like a global function and and can be used anywhere in the site like other wordpress function.
+   ii. The function is having some if conditions for title, subtitle, and background image which will work on passing arguments
+        By the way meanwhile we created two more custom fields for all post type. The fields are 'page banner subtile' and 'page banner background image'
+       conditon - if a page does not have custom title, it will have a fallback title. Same for subtitle too.
+       conditon - if a page does not have custom uploaded background, it will have a fallback image
+   ii. First of all we will use page banner function in page.php file 
